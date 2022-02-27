f1c70a0383eb582f8619348c1a043c56148a725d
Chris James <>
2022-02-27 11:59:08 +0800

    initial setup

---
+++ FlagMaster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlagMaster
+//
+//  Created by Chris James on 27/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
