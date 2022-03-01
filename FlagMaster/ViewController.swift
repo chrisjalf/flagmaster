9a855603f4908df6bfb6196a55b698167d4c9672
Chris James <>
2022-03-01 23:23:30 +0800

    set country name label and score label above stack view, update text of the labels each time when setCountryFlags function is called

--- FlagMaster/ViewController.swift
+++ FlagMaster/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var countryNameLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
     @IBOutlet var imageView1: UIImageView!
     @IBOutlet var imageView2: UIImageView!
     @IBOutlet var imageView3: UIImageView!
@@ -18,6 +20,7 @@ class ViewController: UIViewController {
     var selectedCountriesIndex = [Int]()
     var selectedCountry = Country()
     var selectedCountryIndex = -1
+    var score = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +33,7 @@ class ViewController: UIViewController {
     
     func setCountryFlags() {
         var countryIndexSet = Set<Int>()
+        var selectedCountries = [Country]()
         
         while countryIndexSet.count < 4 {
             let randomIndex = Int(arc4random_uniform(UInt32(countries.count)))
@@ -37,7 +41,6 @@ class ViewController: UIViewController {
         }
         
         selectedCountriesIndex = Array(countryIndexSet)
-        var selectedCountries = [Country]()
         
         for i in 0...selectedCountriesIndex.count - 1 {
             let country = countries[selectedCountriesIndex[i]]
@@ -47,10 +50,19 @@ class ViewController: UIViewController {
         
         selectedCountryIndex = Int.random(in: 0...3)
         selectedCountry = countries[selectedCountriesIndex[selectedCountryIndex]]
+        countryNameLabel.text = selectedCountry.name
+        
+        countryNameLabel.font = countryNameLabel.font.withSize(selectedCountry.name.count > 30 ? 16 : 24)
+        scoreLabel.font = scoreLabel.font.withSize(selectedCountry.name.count > 30 ? 16 : 24)
     }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
         let choiceIndex = buttons.firstIndex(of: sender.titleLabel!.text!)
+        
+        if (choiceIndex == selectedCountryIndex) {
+            score += 1
+            scoreLabel.text = String(score)
+        }
         
         setCountryFlags()
     }
